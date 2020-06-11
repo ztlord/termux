f41b8a0b37437b242e473bfb2f97dd3f90114ac7
ztlord <66460566+ztlord@users.noreply.github.com>
2020-06-11 01:36:20 -0500

    Update confi.sh

--- confi.sh
+++ confi.sh
@@ -8,6 +8,19 @@ clear
 apt -y install proot util-linux coreutils vim python python2 ruby perl clang curl wget make openssl php zip unzip tar htop bison findutils git apr apr-util libtool pkg-config tmux termux-tools ncurses-utils ncurses postgresql termux-elf-cleaner openssl-tool
 pip install mechanize
 pip2 install mechanize
+pip install --upgrade pip
+pip install requests
+pip install phonenumbers
+pip install requests
+pip install scapy
+pip install wget
+pip install argparse
+pkg install tsu
+pkg install root-repo -y
+pkg instal aircrack-ng
+pkg install wget gnupg
+pkg update && pkg upgrade && pkg install x11-repo && pkg install tigervnc openbox obconf xorg-xsetroot xcompmgr xterm polybar st libnl zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings
+
 echo "Instalando Banner"
 cd
 cd ../usr/etc/;ls
