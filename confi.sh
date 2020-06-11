ba4c6f2e623d358334417c9578d044ae2400d6ac
ztlord <66460566+ztlord@users.noreply.github.com>
2020-06-10 22:50:51 -0500

    Update confi.sh

--- confi.sh
+++ confi.sh
@@ -6,7 +6,8 @@ apt -y upgrade
 clear
 apt -y install proot util-linux coreutils vim python python2 ruby perl clang curl wget make openssl php zip unzip tar htop bison findutils git apr apr-util libtool pkg-config tmux termux-tools ncurses-utils ncurses postgresql termux-elf-cleaner openssl-tool
 echo "Instalando Banner"
-cd ../usd/etc/;ls
+cd
+cd ../usr/etc/;ls
 rm motd
 rm bash.bashrc
 ls
